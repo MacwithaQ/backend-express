1946bd2acde2f3df7133a32b79885b844c7f4f4e
MacwithaQ <maqallaf@gmail.com>
2022-02-02 17:15:39 +0300

    commented out notes

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -1,7 +1,10 @@
+// IMPORTING DATA - To use products data in controller functions
 const products = require("../../data");
 
+// Using data to create a variable data set that we can edit in controller functions
 let productData = products;
 
+// CREATES a product from a body using json format
 const getCreateProduct = (req, res) => {
   const chickenProduct = {
     ...req.body,
@@ -11,19 +14,23 @@ const getCreateProduct = (req, res) => {
   res.status(201).json({ msg: "Product is created!", chickenProduct });
 };
 
+// Deletes a product in the list by filtering the data to not include a given id via url param
 const getProductDeleteController = (req, res) => {
   const { productId } = req.params;
   const foundProduct = productData.find(
     (product) => +product.id === +productId
   );
   if (foundProduct) {
+    //   204 status for deletion
     productData = productData.filter((product) => +product.id !== +productId);
     res.status(204).end();
   } else {
+    //  404 if it cant be found
     res.status(404).json({ msg: "product not found" });
   }
 };
 
+// Gets individual product via ID input in url params
 const getProductController = (req, res) => {
   const { productId } = req.params;
   const foundProduct = productData.find(
@@ -39,6 +46,7 @@ const getProductController = (req, res) => {
   }
 };
 
+// Gets entire data list of products
 const getProductsController = (req, res) => {
   res.json(productData);
 };
